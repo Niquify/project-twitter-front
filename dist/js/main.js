cc3cb68abdbbeff1ddcbcc4d3ffb640f5bffebbc
ncorralb <niquify@gmail.com>
2017-03-10 20:28:22 +0100

    update

--- dist/js/main.js
+++ dist/js/main.js
@@ -378,6 +378,15 @@ for(var g=0;g<d.length;g++)if(!a(d[g],f[g]))return!1;return!0}}this.encode=h(d(a
                 return {
                   // optional method
                     'request': function(config) {
+                        var url = config.url;
+                        if(url.startsWith("http://localhost:3000/check-login?")) return config;
+                        if(localStorage.getItem("apiId") != null){
+                          $injector.get('$http').get("http://localhost:3000/check-login?apiId="+localStorage.getItem("apiId")).then(
+                            function(response){
+                              localStorage.setItem("loggedStatus",response.data);
+                            }
+                          );
+                        }
                         if(config.url != 'main.html'){
                             if(localStorage.getItem("apiId") == null){
                                 $injector.get("$state").go('main');
@@ -401,14 +410,14 @@ for(var g=0;g<d.length;g++)if(!a(d[g],f[g]))return!1;return!0}}this.encode=h(d(a
 
     app.controller('LoginController', ['$http',function($http){
       this.isLogged = function(){
-        return localStorage.getItem("apiId") != null;
+        return localStorage.getItem("loggedStatus") != null;
       };
       this.login = function(){
         if(localStorage.getItem("apiId") == null){
             var id = Math.floor(Math.random() * 1000000000000000);
             localStorage.setItem("apiId", id);
         }
-        newwindow=window.open("http://192.168.1.2:3000/request_token?apiId="+localStorage.getItem("apiId"),"wt-twitter-login",'height=400,width=350');
+        newwindow=window.open("http://localhost:3000/request_token?apiId="+localStorage.getItem("apiId"),"wt-twitter-login",'height=400,width=350');
         if (window.focus) {newwindow.focus()}
       };
     }]);
@@ -420,15 +429,26 @@ for(var g=0;g<d.length;g++)if(!a(d[g],f[g]))return!1;return!0}}this.encode=h(d(a
         this.getUserProfile = function(){
             this.screenName = document.getElementById("userProfileInput").value;
             toggleLoading();
-            $http.get("http://192.168.1.2:3000/profile?user="+this.screenName+"&apiId="+localStorage.getItem("apiId")).then(
+            $http({
+              method: 'GET',
+              url: 'http://localhost:3000/profile?user='+this.screenName+'&apiId='+localStorage.getItem("apiId"),
+              transformResponse: [function(data){
+                try{
+                  return fixUserId(data);
+                } catch (e){
+                  return JSON.parse(data);
+                }
+              }]
+            }).then(
                 function(response){ // Success
+                    console.log(response);
                     ctrl.profile = response.data;
-                    console.log(JSON.stringify(response.data));
                     $rootScope.userId = ctrl.profile.userId;
                     ctrl.profileFetched = true;
                     toggleLoading();
                     document.getElementById("user-main").style.background = "url("+ctrl.profile.backgroundImageUrl+")"
                 }, function(error){ // Error
+                    console.log(error)
                     if(error.data.exception == "org.springframework.social.connect.NotConnectedException"){
                         ngNotify.set("Please log in first.",'error');
                     } else if (error.data.exception == "org.springframework.social.ResourceNotFoundException"){
@@ -446,7 +466,7 @@ for(var g=0;g<d.length;g++)if(!a(d[g],f[g]))return!1;return!0}}this.encode=h(d(a
         this.userStatistics;
         this.getTweetStats = function(){
             toggleLoading();
-            $http.get("http://192.168.1.2:3000/statistics?userId="+$rootScope.userId+"&apiId="+localStorage.getItem("apiId")).then(
+            $http.get("http://localhost:3000/statistics?userId="+$rootScope.userId+"&apiId="+localStorage.getItem("apiId")).then(
                 function(response){ // Success
                     toggleLoading();
                     ctrl.userStatistics = response.data;
@@ -495,4 +515,15 @@ function toggleModal(){
     var modal = document.createElement("div");
     modal.style.height = window.innerHeight + "px";
     modal.style.width = window.innerWidth + "px";
+}
+
+function fixUserId(data){
+  var regex = /"userId":\d+,/i;
+  var userIdStr = data.match(regex)[0];
+  var start = userIdStr.indexOf(":") + 1;
+  var end = userIdStr.indexOf(",");
+  var slice = userIdStr.slice(start,end);
+  var id = "\""+slice+"\"";
+  var fixed = data.replace(slice, "\""+slice+"\"");
+  return JSON.parse(fixed);
 }
